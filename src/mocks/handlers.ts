3162eb5de77d1c3cb6097ae0044c77255ca75ee9
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-05 20:03:17 +0100

    Install msw , add handlers , storeMock, setuptest

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,13 +1,8 @@
 import { rest } from "msw";
 
+const url = process.env.REACT_APP_URL_API;
 export const handlers = [
-  rest.get("/login", (req, res, ctx) => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem("is-authenticated", "true");
-
-    return res(
-      // Respond with a 200 status code
-      ctx.status(200)
-    );
+  rest.get(`${url}`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json([]));
   }),
 ];
