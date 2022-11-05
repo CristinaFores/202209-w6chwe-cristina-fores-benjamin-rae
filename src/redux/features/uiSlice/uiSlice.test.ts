2daab84e23333c30d5380298491d2e2568684b69
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-05 17:45:29 +0100

    Add tests for uiReducer show loading and hide loading actions

--- src/redux/features/uiSlice/uiSlice.test.ts
+++ src/redux/features/uiSlice/uiSlice.test.ts
@@ -1,5 +1,10 @@
 import mockInitialUiState from "../../../mocks/mockInitialUiState";
-import { uiReducer } from "./uiSlice";
+import { UiState } from "./types";
+import {
+  hideLoadingActionCreator,
+  showLoadingActionCreator,
+  uiReducer,
+} from "./uiSlice";
 
 describe("Given a uiReducer", () => {
   describe("When it is invoked", () => {
@@ -12,6 +17,41 @@ describe("Given a uiReducer", () => {
         const newUiState = uiReducer(mockInitialUiState, unknownAction);
 
         expect(newUiState).toStrictEqual(mockInitialUiState);
+      });
+    });
+
+    describe("and it receives the initial ui state and a show loading action", () => {
+      test("Then it should return a copy of the initial state with isLoading true", () => {
+        const expectedUiState = {
+          isLoading: true,
+        };
+
+        const newUiState = uiReducer(
+          mockInitialUiState,
+          showLoadingActionCreator()
+        );
+
+        expect(newUiState).toStrictEqual(expectedUiState);
+      });
+    });
+
+    describe("and it receives the initial ui state with loading true and a hide loading action", () => {
+      test("Then it should return a copy of the initial state with isLoading false", () => {
+        const initialUiState: UiState = {
+          ...mockInitialUiState,
+          isLoading: true,
+        };
+        const expectedUiState: UiState = {
+          ...mockInitialUiState,
+          isLoading: false,
+        };
+
+        const newUiState = uiReducer(
+          initialUiState,
+          hideLoadingActionCreator()
+        );
+
+        expect(newUiState).toStrictEqual(expectedUiState);
       });
     });
   });
