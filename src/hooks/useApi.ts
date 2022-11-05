144887922e992c6e479f1d4cbaeb26474f02aaa2
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-05 20:41:45 +0100

    Add show and hide loading dispatch to load all robots

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,6 +1,10 @@
 import { useCallback } from "react";
 import { loadRobotsActionCreator } from "../redux/features/robotsSlice/robotsSlice";
 import { Robots } from "../redux/features/robotsSlice/types";
+import {
+  hideLoadingActionCreator,
+  showLoadingActionCreator,
+} from "../redux/features/uiSlice/uiSlice";
 
 import { useAppDispatch } from "../redux/hooks";
 
@@ -9,11 +13,20 @@ const useApi = () => {
   const url = process.env.REACT_APP_URL_API;
 
   const loadAllRobot = useCallback(async () => {
+    dispatch(showLoadingActionCreator());
     try {
       const response = await fetch(url!);
       const apiResponse: Robots = await response.json();
+
+      if (response.status >= 400) {
+        throw new Error("Couldn't load robots");
+      }
+
       dispatch(loadRobotsActionCreator(apiResponse));
+      dispatch(hideLoadingActionCreator());
     } catch (error: unknown) {
+      dispatch(hideLoadingActionCreator());
+
       throw new Error(`There was an error: ${(error as Error).message}`);
     }
   }, [url, dispatch]);
