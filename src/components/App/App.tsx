b4d902faee8b96f28c150e98df6f65afd0bb1399
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-06 10:00:51 +0100

    Add custom hook to App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
+import useApi from "../../hooks/useApi";
 import { useAppSelector } from "../../redux/hooks";
 import Loading from "../Loading/Loading";
 import AppStyled from "./AppStyled";
 
 function App() {
   const isLoading = useAppSelector((state) => state.ui.isLoading);
+  const { loadAllRobots } = useApi();
+
+  useEffect(() => {
+    loadAllRobots();
+  }, [loadAllRobots]);
 
   return <AppStyled>{isLoading && <Loading />}</AppStyled>;
 }
