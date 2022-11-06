be789c10bec669bf9ce4c9bc366dd75fdd155939
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-11-06 12:48:15 +0100

    Create robots list component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,11 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router";
 import HomePage from "../../pages/HomePage/HomePage";
-import useApi from "../../hooks/useApi";
 import { useAppSelector } from "../../redux/hooks";
 import Loading from "../Loading/Loading";
 import AppStyled from "./AppStyled";
 
 function App() {
   const isLoading = useAppSelector((state) => state.ui.isLoading);
-  const { loadAllRobots } = useApi();
-
-  useEffect(() => {
-    loadAllRobots();
-  }, [loadAllRobots]);
 
   return (
     <AppStyled>
---
+++ src/components/RobotsList/RobotsListStyled.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const RobotsListStyled = styled.main``;
+
+export default RobotsListStyled;
