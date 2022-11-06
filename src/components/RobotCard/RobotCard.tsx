e5cfff0eebd88318f4b72e8047167b549fb15c85
Cristina Fores <cristinaforescampos1992@gmail.com>
2022-11-06 11:22:01 +0100

    Create RobotCard

---
+++ src/components/RobotCard/RobotCard.tsx
@@ -0,0 +1,31 @@
+import { Robot } from "../../redux/features/robotsSlice/types";
+import Button from "../Button/Button";
+import { RobotCardStyled } from "./RobotCardStyled";
+
+interface RobotCardProps {
+  robot: Robot;
+}
+
+const RobotCard = ({
+  robot: { image, name, speed, createdOn, strength },
+}: RobotCardProps): JSX.Element => {
+  return (
+    <RobotCardStyled className="card-robot">
+      <div className="card-robot__image-container">
+        <img className="card-robot__image" src={`${image}`} alt={name} />
+        <ul className="card-robot__info">
+          <li className="card-robot__text">{`🏃‍♂️: ${speed}`} </li>
+          <li className="card-robot__text">{`💪: ${strength}`}</li>
+          <li className="card-robot__text">{`📆: ${createdOn}`}</li>
+        </ul>
+      </div>
+      <h2 className="card-robot__title">{`${name}`}</h2>
+      <div className="card-robot__buttons">
+        <Button text="Edit" action={() => {}} />
+        <Button text="Delete" action={() => {}} />
+      </div>
+    </RobotCardStyled>
+  );
+};
+
+export default RobotCard;
